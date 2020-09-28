06fc7ef577fd5096cd9edf8c541758eb5843e4ac
Andre Teles <andre.telestp@gmail.com>
2020-09-28 18:52:11 -0300

    refactor: add group route

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,11 @@
 
 Auth::routes();
 
-Route::get('/projects', 'ProjectsController@index')->middleware('auth');
-Route::get('/projects/{project}', 'ProjectsController@show')->middleware('auth');
-Route::post('/projects', 'ProjectsController@store')->middleware('auth');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/projects', 'ProjectsController@index');
+    Route::get('/projects/{project}', 'ProjectsController@show');
+    Route::post('/projects', 'ProjectsController@store');
+    Route::get('/home', 'HomeController@index')->name('home');
+});
 
 Route::get('/home', 'HomeController@index')->name('home');
