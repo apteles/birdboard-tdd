44702a07643ae8eac707c5dd5a33d70777cef2cb
Andre Teles <andre.telestp@gmail.com>
2020-09-28 19:30:00 -0300

    chore: add page create project

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -14,14 +14,17 @@ class ProjectsController extends Controller
         return view('projects.index', \compact('projects'));
     }
 
+    public function create()
+    {
+        return view('projects.create');
+    }
+
     public function store()
     {
         $attributes = request()->validate([
             'title' => 'required',
             'description' => 'required'
         ]);
-
-        //$attributes['owner_id'] = auth()->id();
 
         auth()->user()->projects()->create($attributes);
 
@@ -30,9 +33,10 @@ class ProjectsController extends Controller
 
     public function show(Project $project)
     {
-        // if (auth()->id() !== $project->owner_id) {
-        //     abort(403);
-        // }
+        if (auth()->user()->isNot($project->owner)) {
+            abort(403);
+        }
+
         return view('projects.show', \compact('project'));
     }
 }
